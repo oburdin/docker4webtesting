db26aed3b0e3a80d76f1bb50c49e9953ef419bd9
Oleksii Burdin <oburdin@cogmiance.com>
2016-01-29 15:35:22 +0200

    myproject directory as a place-holder for your PHP project to run.

--- drupal_mysql/clear_docker_images.sh
+++ drupal_mysql/clear_docker_images.sh
@@ -1,6 +1,6 @@
-# Delete all docker containers by mask "ago"
+# Delete docker containers by mask "ago"
 sudo docker ps -a | grep 'ago' | awk '{print $1}' | sudo xargs --no-run-if-empty docker rm
 
-# Delete docker images by mask "mask"
-sudo docker images | grep 'mask' | awk '{print $3}' | sudo xargs --no-run-if-empty docker rmi
+# Delete docker images by mask "unity"
+sudo docker images | grep 'db' | awk '{print $3}' | sudo xargs --no-run-if-empty docker rmi
 
